7e19f2e543f203220a7dc1f2e2dc0bd32a805cc5
main <prince.vermandere@outlook.be>
2021-11-12 08:49:46 +0100

    Integeration Test voor method verhoogVerkoopprijs

--- test/java/com/example/jpa/services/DefaultArtikelServiceTest.java
+++ test/java/com/example/jpa/services/DefaultArtikelServiceTest.java
@@ -1,18 +1,38 @@
 package com.example.jpa.services;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.example.jpa.repositories.JpaArtikelRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @Import(DefaultArtikelService.class)
-@ComponentScan(resourcePattern = "repositories", c)
-class DefaultArtikelServiceTest {
+@ComponentScan(value = "com.example.jpa.repositories",resourcePattern = "JpaArtikelRepository.class")
+@Sql("/ArtikelRepositorySql.sql")
+class DefaultArtikelServiceTest extends AbstractTransactionalJUnit4SpringContextTests {
+    private final DefaultArtikelService service;
+    private final EntityManager manager;
 
-    @BeforeEach
-    void setUp() {
+    DefaultArtikelServiceTest(DefaultArtikelService service, EntityManager manager) {
+        this.service = service;
+        this.manager = manager;
+    }
+
+    long getId() {
+        return jdbcTemplate.queryForObject("select id from artikels where naam = 'kruiden'", Long.class);
+    }
+    @Test
+    void verhoogVerkoopprijs(){
+        service.verhoogPrijs(getId(), BigDecimal.ONE);
+        manager.flush();
+        assertThat(jdbcTemplate.queryForObject("select verkoopprijs from artikels where id = ?", BigDecimal.class, getId())).isEqualByComparingTo("2.5");
     }
 }
